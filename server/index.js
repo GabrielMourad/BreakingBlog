1ba7bedcd2af73584420b531f0cf3c0fb4a71b0c
GabrielMourad <gabemrad@yahoo.com>
2023-12-17 13:01:55 -0800

    removed port

--- server/index.js
+++ server/index.js
@@ -92,6 +92,3 @@ server.get('/post/:id', async (request, response) => {
     response.json(postDoc)
 })
 
-// Start the server and listen on port 3000
-console.log('Server listening on port 3000')
-server.listen(3000)

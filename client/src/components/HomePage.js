57d6ac26a0e15c1268057c50d57fa5ef52b1a55a
GabrielMourad <gabemrad@yahoo.com>
2023-12-18 00:46:42 -0800

    mdb test

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,14 +1,28 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Post from './Post';
-import { BlogContext } from '../context-api/BlogContext';
 
 // HomePage component displays a list of posts retrieved from the server
 export default function HomePage() {
   // State to store the list of posts
-  
-  const {posts} = useContext(BlogContext)
+  const [posts, setPosts] = useState([]);
 
-  
+  // useEffect hook to fetch posts when the component mounts
+  useEffect(() => {
+    // Fetch posts from the server
+    fetch('https://breaking-blog-server.vercel.app/post')
+      .then((response) => {
+        // Parse the response as JSON
+        response.json().then((posts) => {
+          // Update the state with the fetched posts
+          setPosts(posts);
+        });
+      })
+      .catch((error) => {
+        // Log an error if there's an issue with fetching posts
+        console.error('Error fetching posts:', error);
+      });
+  }, []);
+
   // Log the posts to the console for debugging
   console.log(posts);
 
